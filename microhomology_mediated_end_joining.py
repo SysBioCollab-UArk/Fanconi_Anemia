38162bae328d4f265eef09fd1af6f945cf35e3f8
andrearichards <andreashalini@gmail.com>
2025-06-14 16:05:19 -0500

    Updated the model to include MRN formation rules and RPA binding rules.

--- microhomology_mediated_end_joining.py
+++ microhomology_mediated_end_joining.py
@@ -20,63 +20,87 @@ Model()
 Monomer("DSB", ["b", "b"])
 Monomer("Parp1", ["dsb", "ctip_mre11"])
 Monomer("CtIP", ["parp1"])
-Monomer("MRE11", ["parp1"])
+Monomer("MRE11", ["parp1_rad50"])
+# Monomer("MRN", ["dsb"])
+Monomer("RAD50", ["mre11", "nbs1"])
+Monomer("NBS1", ["rad50"])
+Monomer("RPA", ["dsb"])
 
 Parameter("DSB_0", 100)
 Parameter("Parp1_0", 100)
 Parameter("CtIP_0", 100)
 Parameter("MRE11_0", 100)
+# Parameter("MRN_0", 100)
+Parameter("RAD50_0", 100)
+Parameter("NBS1_0", 100)
+Parameter("RPA_0", 100)
 
 Initial(DSB(b=MultiState(None, None)), DSB_0)
 Initial(Parp1(dsb=None, ctip_mre11=None), Parp1_0)
 Initial(CtIP(parp1=None), CtIP_0)
-Initial(MRE11(parp1=None), MRE11_0)
+Initial(MRE11(parp1_rad50=None), MRE11_0)
+Initial(RAD50(mre11=None, nbs1=None), RAD50_0)
+Initial(NBS1(rad50=None), NBS1_0)
+Initial(RPA(dsb=None), RPA_0)
+
 
 Parameter("kf_DSB_Parp1", 1)
 Parameter("kr_DSB_Parp1", 10)
-Parameter("kf_DSB_CtIP", 1)
-Parameter("kr_DSB_CtIP", 10)
-
 Rule("Parp1_binds_DSB",
-      DSB(b=None) + Parp1(dsb=None, ctip_mre11=None) |
-      DSB(b=1) % Parp1(dsb=1, ctip_mre11=None), kf_DSB_Parp1, kr_DSB_Parp1)
-
-# TODO: Need to deal with unbinding correctly - worry about this later
-Rule("CtIP_binds_Parp1",
-      CtIP(parp1=None) + DSB(b=1) % Parp1(dsb=1, ctip_mre11=None) |
-      CtIP(parp1=2) % DSB(b=1) % Parp1(dsb=1, ctip_mre11=2), kf_DSB_CtIP, kr_DSB_CtIP)
-
-Rule("MRE11_binds_Parp1",
-     MRE11(parp1=None) +
-     DSB(b=MultiState(1,3)) % Parp1(dsb=1, ctip_mre11=None) % Parp1(dsb=3, ctip_mre11= 2) % CtIP(parp1=2) |
-     MRE11(parp1=4) %
-     DSB(b=MultiState(1,3)) % Parp1(dsb=1, ctip_mre11=4) % Parp1(dsb=3, ctip_mre11= 2) % CtIP(parp1=2),
+     DSB(b=None) + Parp1(dsb=None, ctip_mre11=None) |
+     DSB(b=1) % Parp1(dsb=1, ctip_mre11=None),
      kf_DSB_Parp1, kr_DSB_Parp1)
 
-# Next step in which the resection process begins to create ssDNA.
-class ResectionProcess:
-     def __init__(self, ssDNA_overhang_length, ctip_active, mre11_active, exo1_active, dna2_active):
-          self.ssDNA_overhang_length = ssDNA_overhang_length  # Initial length of ssDNA overhang
-          self.ctip_active = ctip_active  # True if CtIP is active
-          self.mre11_active = mre11_active  # True if MRE11 is active
-          self.exo1_active = exo1_active  # True if EXO1 is active
-          self.dna2_active = dna2_active  # True if DNA2 is active
+# TODO: Need to deal with unbinding correctly - worry about this later
+Parameter("kf_DSB_CtIP", 1)
+Parameter("kr_DSB_CtIP", 10)
+Rule("CtIP_binds_Parp1",
+     CtIP(parp1=None) + DSB(b=1) % Parp1(dsb=1, ctip_mre11=None) |
+     CtIP(parp1=2) % DSB(b=1) % Parp1(dsb=1, ctip_mre11=2),
+     kf_DSB_CtIP, kr_DSB_CtIP)
 
-     def start_resection(self):
-          # CtIP and MRE11 initiate the resection
-          if self.ctip_active and self.mre11_active:
-               print("CtIP and MRE11 initiating resection... Trimming 5' ends.")
-               self.ssDNA_overhang_length += 10  # CtIP and MRE11 trim some of the 5' ends
-          # EXO1 extends the resection
-          if self.exo1_active:
-               print("EXO1 extending resection... Creating longer ssDNA overhangs.")
-               self.ssDNA_overhang_length += 20  # EXO1 extends the ssDNA overhangs
-          # DNA2 continues the resection process
-          if self.dna2_active:
-               print("DNA2 helicase assisting in resection... Further elongating ssDNA overhangs.")
-               self.ssDNA_overhang_length += 25  # DNA2 continues to process the overhangs
+Parameter("kf_mre11_Parp1",1)
+Parameter("kr_mre11_Parp1",10)
+Rule("MRE11_binds_Parp1",
+     MRE11(parp1_rad50=None) +
+     DSB(b=MultiState(1,3)) % Parp1(dsb=1, ctip_mre11=None) % Parp1(dsb=3, ctip_mre11= 2) % CtIP(parp1=2) |
+     MRE11(parp1_rad50=4) %
+     DSB(b=MultiState(1,3)) % Parp1(dsb=1, ctip_mre11=4) % Parp1(dsb=3, ctip_mre11= 2) % CtIP(parp1=2),
+     kf_mre11_Parp1, kr_mre11_Parp1)
 
-          print(f"Resection completed. ssDNA overhang length: {self.ssDNA_overhang_length} nucleotides.")
+Parameter("kf_mre11_binds_rad50", 1)
+Parameter("kr_mre11_binds_rad50",1)
+Rule("MRE11_binds_RAD50",
+     MRE11(parp1_rad50=None) + RAD50(mre11=None, nbs1=None) |
+     MRE11(parp1_rad50=1) % RAD50(mre11=1, nbs1=None),
+     kf_mre11_binds_rad50, kr_mre11_binds_rad50)
+
+Parameter("kf_nbs1_binds_mre11_rad50", 1)
+Parameter("kr_nbs1_binds_mre11_rad50", 1)
+Rule("NBS1_binds_MRE11_RAD50",
+     MRE11(parp1_rad50=1) % RAD50(mre11=1, nbs1=None) + NBS1(rad50=None) |
+     MRE11(parp1_rad50=1) % RAD50(mre11=1, nbs1=2) % NBS1(rad50=2),
+     kf_nbs1_binds_mre11_rad50, kr_nbs1_binds_mre11_rad50)
+
+# Parameter("kf_mrn_formation", 1)
+# Parameter("kr_mrn_formation", 1)
+# Rule("MRN_complex_formation",
+#      MRE11(parp1_rad50=None) + RAD50(mre11=None, nbs1=None) + NBS1(rad50=None) |
+#      MRE11(parp1_rad50=1) % RAD50(mre11=1, nbs1=2) % NBS1(rad50=2),
+#      kf_mrn_formation, kr_mrn_formation)
+
+Parameter("k_RPA_binds_MRE11_Parp1_DSB", 1)
+Rule("RPA_binds_MRE11_Parp1_DSB",
+     RPA(dsb=None) + MRE11(parp1_rad50=4) % DSB(b=MultiState(1, ANY)) % Parp1(dsb=1, ctip_mre11=4) >>
+     RPA(dsb=1) % DSB(b=MultiState(1, ANY)) + MRE11(parp1_rad50=None) + Parp1(dsb=None, ctip_mre11=None),
+     k_RPA_binds_MRE11_Parp1_DSB)
+
+Parameter("k_RPA_binds_CtIP_Parp1_DSB", 1)
+Rule("RPA_binds_CtIP_Parp1_DSB",
+     RPA(dsb=None) + CtIP(parp1=4) % DSB(b=MultiState(1, ANY)) % Parp1(dsb=1, ctip_mre11=4) >>
+     RPA(dsb=1) % DSB(b=MultiState(1, ANY)) + CtIP(parp1=None) + Parp1(dsb=None, ctip_mre11=None),
+     k_RPA_binds_CtIP_Parp1_DSB)
+
 
 print(model)
 print(model.monomers)
