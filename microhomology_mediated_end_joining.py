b4cc6153353563c863a05f8549331d1fbc9befa2
andrearichards <andreashalini@gmail.com>
2025-09-27 17:02:18 -0500

    Updated the model to fix the problems in the previous version. These included fixing rules that allowed species to be created that shouldn't exist.

--- microhomology_mediated_end_joining.py
+++ microhomology_mediated_end_joining.py
@@ -29,14 +29,14 @@ Monomer("LIG3", ["dsb", "dsb"])
 
 
 Parameter("DSB_0", 100)
-Parameter("Parp1_0", 100)
-Parameter("CtIP_0", 100)
-Parameter("MRE11_0", 100)
+Parameter("Parp1_0", 1000)
+Parameter("CtIP_0", 1000)
+Parameter("MRE11_0", 1000)
 Parameter("RAD50_0", 100)
 Parameter("NBS1_0", 100)
-Parameter("RPA_0", 100)
-Parameter("PolQ_0", 100)
-Parameter("LIG3_0", 100)
+Parameter("RPA_0", 1000)
+Parameter("PolQ_0", 1000)
+Parameter("LIG3_0", 1000)
 
 Initial(DSB(b=MultiState(None, None)), DSB_0)
 Initial(Parp1(dsb=None, ctip_mre11=None), Parp1_0)
@@ -65,26 +65,33 @@ Rule("NBS1_binds_MRE11_RAD50",
      MRE11(parp1_rad50=1) % RAD50(mre11=1, nbs1=2) % NBS1(rad50=2),
      kf_nbs1_binds_mre11_rad50, kr_nbs1_binds_mre11_rad50)
 
-# STEP 1: Parp1 binds to DSB
+# STEP 1a: Parp1 binds to DSB
 Parameter("kf_DSB_Parp1", 1)
 Parameter("kr_DSB_Parp1", 10)
 Rule("Parp1_binds_DSB",
-     DSB(b=None) + Parp1(dsb=None, ctip_mre11=None) | DSB(b=1) % Parp1(dsb=1, ctip_mre11=None),
-     kf_DSB_Parp1, kr_DSB_Parp1)
+     DSB(b=None) + Parp1(dsb=None, ctip_mre11=None) >> DSB(b=1) % Parp1(dsb=1, ctip_mre11=None),
+     kf_DSB_Parp1)
 
-# STEP 2a: Parp1 recruits CtIP
+# STEP 1b: Parp1 unbinds from DSB
+Rule("Parp1_unbinds_DSB",
+     DSB(b=MultiState(1,None)) % Parp1(dsb=1, ctip_mre11=None) >>
+     DSB(b=MultiState(None,None)) + Parp1(dsb=None, ctip_mre11=None),
+     kr_DSB_Parp1)
+
+# STEP 1c: Parp1 unbinds from DSB-Parp1
+Rule("Parp1_unbinds_DSB_Parp1",
+     DSB(b=MultiState(1,2)) % Parp1(dsb=1, ctip_mre11=None) % Parp1(dsb=2, ctip_mre11=None) >>
+     DSB(b=MultiState(1,None)) % Parp1(dsb=1, ctip_mre11=None) + Parp1(dsb=None, ctip_mre11=None),
+     kr_DSB_Parp1)
+
+# STEP 2: Parp1 recruits CtIP
 Parameter("kf_DSB_CtIP", 1)
 Parameter("kr_DSB_CtIP", 10)
-Rule("CtIP_binds_Parp1",
-     CtIP(parp1=None) + DSB(b=1) % Parp1(dsb=1, ctip_mre11=None) >>
-     CtIP(parp1=2) % DSB(b=1) % Parp1(dsb=1, ctip_mre11=2),
-     kf_DSB_CtIP)
-
-# STEP 2b: CtIP unbinds Parp1 (only if MRE11 is not bound)
 Rule("CtIP_unbinds_Parp1",
-     DSB(b=MultiState(1,2)) % Parp1(dsb=1, ctip_mre11=None) % Parp1(dsb=2, ctip_mre11=3) % CtIP(parp1=3) >>
-     DSB(b=MultiState(1,2)) % Parp1(dsb=1, ctip_mre11=None) % Parp1(dsb=2, ctip_mre11=None) + CtIP(parp1=None),
-     kr_DSB_CtIP)
+     CtIP(parp1=None) +
+     DSB(b=MultiState(1,2)) % Parp1(dsb=1, ctip_mre11=None) % Parp1(dsb=2, ctip_mre11=None) |
+     DSB(b=MultiState(1,2)) % Parp1(dsb=1, ctip_mre11=None) % Parp1(dsb=2, ctip_mre11=3) % CtIP(parp1=3),
+     kf_DSB_CtIP, kr_DSB_CtIP)
 
 # STEP 3: Parp1 recruits MRE11 after CtIP is bound
 Parameter("kf_mre11_Parp1",1)
@@ -99,25 +106,34 @@ Rule("MRE11_binds_Parp1",
 # STEP 4a: RPA displaces Parp1 % CtIP
 Parameter("k_RPA_binds_CtIP_Parp1_DSB", 1)
 Rule("RPA_binds_CtIP_Parp1_DSB",
-     RPA(dsb=None, parp1=None) + CtIP(parp1=4) % DSB(b=MultiState(1, ANY)) % Parp1(dsb=1, ctip_mre11=4) >>
-     RPA(dsb=1, parp1=2) % DSB(b=MultiState(1, ANY)) % CtIP(parp1=4) % Parp1(dsb=2, ctip_mre11=4),
+     RPA(dsb=None, parp1=None) + DSB(b=MultiState(1, ANY))
+     % Parp1(dsb=1, ctip_mre11=3) % CtIP(parp1=3) % MRE11(parp1_rad50=ANY) >>
+     RPA(dsb=1, parp1=5) % DSB(b=MultiState(1, ANY))
+     % Parp1(dsb=5, ctip_mre11=3) % CtIP(parp1=3) % MRE11(parp1_rad50=ANY),
      k_RPA_binds_CtIP_Parp1_DSB)
 
 # STEP 4b: RPA displaces Parp1 % MRE11
 Parameter("k_RPA_binds_MRE11_Parp1_DSB", 1)
 Rule("RPA_binds_MRE11_Parp1_DSB",
-     RPA(dsb=None, parp1=None) + MRE11(parp1_rad50=4) % DSB(b=MultiState(1, ANY)) % Parp1(dsb=1, ctip_mre11=4) >>
-     RPA(dsb=1,parp1=2) % DSB(b=MultiState(1, ANY)) % MRE11(parp1_rad50=4) % Parp1(dsb=2, ctip_mre11=4),
+     RPA(dsb=None, parp1=None) + DSB(b=MultiState(1, ANY)) % Parp1(dsb=1, ctip_mre11=4) % MRE11(parp1_rad50=4) >>
+     RPA(dsb=1,parp1=2) % DSB(b=MultiState(1, ANY)) % Parp1(dsb=2, ctip_mre11=4) % MRE11(parp1_rad50=4),
      k_RPA_binds_MRE11_Parp1_DSB)
 
 
 # STEP 5: POLQ displaces RPA
 Parameter("k_PolQ_displaces_RPA", 1)
-Rule("POLQ_displaces_RPA_Parp1",
-     PolQ(dsb=None, parp1=None, polq=None) +
-     DSB(b=MultiState(1, ANY)) % Parp1(dsb=2, ctip_mre11=ANY) % RPA(dsb=1, parp1=2) >>
-     PolQ(dsb=1, parp1=2, polq=None) %
-     DSB(b=MultiState(1, ANY)) % Parp1(dsb=2, ctip_mre11=ANY) + RPA(dsb=None, parp1=None),
+Rule("POLQ_displaces_RPA_Parp1_Parp1",
+     PolQ(dsb=None, parp1=None, polq=None) + RPA(dsb=3) %
+     DSB(b=MultiState(1, 3)) % Parp1(dsb=2, ctip_mre11=ANY) % RPA(dsb=1, parp1=2) >>
+     PolQ(dsb=1, parp1=2, polq=None) % RPA(dsb=3) %
+     DSB(b=MultiState(1, 3)) % Parp1(dsb=2, ctip_mre11=ANY) + RPA(dsb=None, parp1=None),
+     k_PolQ_displaces_RPA)
+
+Rule("POLQ_displaces_RPA_Parp1_POLQ",
+     PolQ(dsb=None, parp1=None, polq=None) + PolQ(dsb=3) %
+     DSB(b=MultiState(1, 3)) % Parp1(dsb=2, ctip_mre11=ANY) % RPA(dsb=1, parp1=2) >>
+     PolQ(dsb=1, parp1=2, polq=None) % PolQ(dsb=3) %
+     DSB(b=MultiState(1, 3)) % Parp1(dsb=2, ctip_mre11=ANY) + RPA(dsb=None, parp1=None),
      k_PolQ_displaces_RPA)
 
 #STEP 6: POLQ aligns microhomologies and extends DNA (POLQs bind to each other)
@@ -126,6 +142,8 @@ Rule("PolQ_aligns_microhomologies",
      PolQ(dsb=ANY, parp1=ANY, polq=None) % PolQ(dsb=ANY, parp1=ANY, polq=None) >>
      PolQ(dsb=ANY, parp1=ANY, polq=1) % PolQ(dsb=ANY, parp1=ANY, polq=1),
      k_PolQ_aligns_microhomolgies)
+
+
 
 #STEP 7: LIG3 binds DSB and seals nicks
 Parameter("k_LIG3_binds_DSB", 1)
@@ -153,7 +171,7 @@ Observable("DSB_tot",DSB())
 # print(model.parameters)
 # print(model.rules)
 
-tspan=np.linspace(0,1000,1001)
+tspan=np.linspace(0,10,1001)
 sim=ScipyOdeSimulator(model, tspan=tspan, verbose=True)
 output=sim.run()
 
@@ -165,3 +183,6 @@ plt.ylabel("concentration")
 plt.legend(loc="best")
 
 plt.show()
+
+for sp in model.species:
+     print(sp)
